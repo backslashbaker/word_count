8d5e8b998ff9514c1a5e005140961509cb0785d9
Derek Baker <derek.baker89@gmail.com>
2020-09-17 09:34:53 +0100

    Create file structure and first test

---
+++ spec/word_count_spec.rb
@@ -0,0 +1,11 @@
+require_relative ('../word_count')
+
+describe WordCount do
+    context “...” do
+       it "..." do
+          hw = HelloWorld.new
+          message = hw.say_hello
+          expect(message).to eq "Hello World!"
+       end
+    end
+ end
