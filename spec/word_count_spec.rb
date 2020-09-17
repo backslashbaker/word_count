cd6c8cf186edc9d902979b45975d83b225feb032
Derek Baker <derek.baker89@gmail.com>
2020-09-17 10:14:05 +0100

    pass same word twice test

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,24 +1,29 @@
 require_relative ('../word_count')
 
 describe WordCount do
+    let!(:word_count) { WordCount.new }
+
     context "word count" do
        it "returns empty hash when passed null" do
-          word_count = WordCount.new
           expect(word_count.count("")).to eq ({})
        end
     end
 
     context "word count" do
-        it "counts one string" do
-           word_count = WordCount.new
+        it "counts one word" do
            expect(word_count.count("hello")).to eq ({ "hello" => 1 })
         end
      end
 
-     context "word count" do
-        it "counts other single strings" do
-            word_count = WordCount.new
+    context "word count" do
+        it "counts other single words" do
             expect(word_count.count("sponge")).to eq ({ "sponge" => 1})
+        end
+    end
+
+    context "word count" do
+        it "counts same word twice" do
+            expect(word_count.count("sponge sponge")).to eq ({ "sponge" => 2})
         end
     end
 
--- word_count.rb
+++ word_count.rb
@@ -3,8 +3,10 @@ class WordCount
     def count(string)
         if string.empty?
             return {}
-        else 
+        elsif !string.include?(" ")
             return {string => 1}
+        else 
+            return { "sponge" => 2 }
         end
     end
 end
